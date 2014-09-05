a91ad650f3dbcce2ef7de5fb8c8cfb69cfa6f6df
Austin Steward <austinsteward@aol.com>
2014-09-05 12:48:38 -0700

    Add strong params to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,19 +19,10 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+
+private
+
+  def answer_params
+    params.require(:post).permit(:text)
+  end
 end
-
-
-@user = current_user
-    @question = Question.find(params[:question_id])
-    @answer = @question.answers.new(answer_params)
-    if @answer.save
-      flash.now.notice = "Successfully saved answer"
-      respond_to do |format|
-        format.html { redirect_to new_question_answer_path(@question) }
-        format.js
-      end
-    else
-      flash.now.alert = "Incomplete!!!!!!!!!!"
-      render 'new'
-    end
