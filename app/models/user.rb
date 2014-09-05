1d8371f4917a62d4073a6b1edc649da0e2e541a1
Austin Steward <austinsteward@aol.com>
2014-09-05 10:12:53 -0700

    Create User class

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :posts
+  validates :username, presence: true
+  validates :handle, presence: true
+  validates :email, presence: true
+end
