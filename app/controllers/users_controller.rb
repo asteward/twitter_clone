3aa8a144163c11956b26d7f1aa01528d7cdba721
Austin Steward <austinsteward@aol.com>
2014-09-05 10:21:26 -0700

    Add Create User function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Thanks for signing up, <strong>#{@user.username}!</strong>".html_safe
+      redirect_to posts_path
+    else
+      flash[:alert] = "<strong>Whoa!</strong> Let's take another look...".html_safe
+      render 'new'
+    end
+  end
+
+
 end
