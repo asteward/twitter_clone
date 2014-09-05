43852a7bfe11c3ba11d8b8414c6a5405c78302ba
Austin Steward <austinsteward@aol.com>
2014-09-05 11:06:43 -0700

    Add New Session function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :posts
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :handle, presence: true
   validates :email, presence: true
 end
