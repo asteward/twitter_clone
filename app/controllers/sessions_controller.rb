5b37cdd98c5b2efba2ec4b2d4b204b278da703c6
Austin Steward <austinsteward@aol.com>
2014-09-05 11:07:11 -0700

    Add Create Session function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,16 @@ class SessionsController < ApplicationController
 
   def new
   end
-  
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "<strong>Hey, #{user.username}!</strong> Check out our cool new posts!".html_safe
+      redirect_to posts_path
+    else
+      flash.now.alert = "<strong>Hey, now -</strong> Check that username and password again.".html_safe
+      render 'new'
+    end
+  end
 end
